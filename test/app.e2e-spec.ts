7a8650e10a5510fd3cc0b48e770bd11fa9cf8757
Abdulloh Alimov <Abdulloh.Alimov@itechart-group.com>
2022-06-09 18:25:03 +0500

    test structure done

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,12 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { PrismaService } from './../src/prisma/prisma.service';
 import { AppModule } from './../src/app.module';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService
+
   beforeAll(async () => {
     const moduleRef = 
       await Test.createTestingModule({
@@ -14,10 +17,37 @@ describe('App e2e', () => {
       whitelist: true,  // No other fields which we dont define
     }));
     await app.init();
+
+    prisma = app.get(PrismaService)
+    await prisma.cleanDb()
   });
 
   afterAll(() => {
     app.close();
+  });
+
+  describe('Auth', () => {
+    describe('Signup', () => {});
+
+    describe('Signin', () => {});
+  });
+
+  describe('User', () => {
+    describe('Get me', () => {});
+
+    describe('Edit user', () => {});
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create bookmark', () => {});
+
+    describe('Get bookmarks', () => {});
+
+    describe('Get bookmark by id', () => {});
+
+    describe('Edit bookmarks', () => {});
+
+    describe('Delete bookmarks', () => {});
   });
 
   it.todo('should pass')
