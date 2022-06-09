480a92a3d6961157b75a48fc9747114fd18fd17a
Abdulloh Alimov <Abdulloh.Alimov@itechart-group.com>
2022-06-09 17:42:42 +0500

    test-db added with dotenv library

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,24 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from './../src/app.module';
 
 describe('App e2e', () => {
+  let app: INestApplication;
   beforeAll(async () => {
     const moduleRef = 
       await Test.createTestingModule({
         imports: [AppModule],
       }).compile();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({  // Setting validation globally
+      whitelist: true,  // No other fields which we dont define
+    }));
+    await app.init();
   });
+
+  afterAll(() => {
+    app.close();
+  });
+
   it.todo('should pass')
 });
